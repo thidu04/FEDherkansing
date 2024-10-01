f184cf7a6a0bf69f160538839b1db62742db4354
thidu04 <thidu04@gmail.com>
2024-10-01 18:25:31 +0200

    updatee

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,9 @@
 // JavaScript Document
 console.log("hi");
+
+var swiper = new Swiper("section", {
+    navigation: {
+        nextEl: "section button:nth-of-type(2)",
+        prevEl: "section button:nth-of-type(1)",
+    },
+});
